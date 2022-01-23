75ae92028a16b158657dd1e502d52232679ac4a5
Engin Bolat <engi.bolat@gmail.com>
2022-01-23 11:57:14 +0300

    Horizontal Listview

--- test_area/lib/main.dart
+++ test_area/lib/main.dart
@@ -1,6 +1,7 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'card_control.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,18 +11,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("TestArea"),
-          centerTitle: true,
-        ),
-        body: Container(
-          color: Colors.green,
-          child: Material(
-            child: ListTile(
-              title: const Text('ListTile with red background'),
-              tileColor: Colors.red,
+          toolbarHeight: 60,
+          title: Text("Shoe Shop",textAlign: TextAlign.start,style:GoogleFonts.getFont("Sedgwick Ave",fontWeight: FontWeight.bold,fontSize: 24),),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(20),
             ),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Container(
+            height: 200,
+            width: double.infinity,
+            child: CardControl(),
           ),
         ),
       ),
