94ea60d78de4a6abe1740e03a265910eec4373d3
Engin Bolat <engi.bolat@gmail.com>
2022-02-14 22:41:50 +0300

    Starting Home Page

--- test_area/lib/home_page.dart
+++ test_area/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_area/main.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,20 +13,202 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
-        centerTitle:true,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: const Text(
+          "Home !!",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const MyApp()));
+            },
+          )
+        ],
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+             const Padding(
+               padding: EdgeInsets.all(8.0),
+               child: Align(
+                alignment: Alignment.centerLeft,
+                  child:  Text(
+                "1.SATIR",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                textAlign: TextAlign.left,
+            )),
+             ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("You Tapped the red box");
+                    },
+                    child: const SizedBox(
+                      width: 200,
+                      height: 300,
+                      child: Card(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("You Tapped the red box");
+                    },
+                    child: const SizedBox(
+                      width: 200,
+                      height: 300,
+                      child: Card(
+                        color: Colors.orange,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("You Tapped the red box");
+                    },
+                    child: const SizedBox(
+                      width: 200,
+                      height: 300,
+                      child: Card(
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //2.SATIR
+            const Padding(
+               padding:  EdgeInsets.all(8.0),
+               child: Align(
+                alignment: Alignment.centerLeft,
+                  child:  Text(
+                "2.SATIR",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                textAlign: TextAlign.left,
+            )),
+             ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("You Tapped the red box");
+                    },
+                    child: const SizedBox(
+                      width: 200,
+                      height: 300,
+                      child: Card(
+                        color: Colors.indigo,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("You Tapped the red box");
+                    },
+                    child: const SizedBox(
+                      width: 200,
+                      height: 300,
+                      child: Card(
+                        color: Colors.lime,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("You Tapped the red box");
+                    },
+                    child: const SizedBox(
+                      width: 200,
+                      height: 300,
+                      child: Card(
+                        color: Colors.blueAccent,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //3. SATIR
+            const Padding(
+               padding:  EdgeInsets.all(8.0),
+               child: Align(
+                alignment: Alignment.centerLeft,
+                  child:  Text(
+                "3.SATIR",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                textAlign: TextAlign.left,
+            )),
+             ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("You Tapped the red box");
+                    },
+                    child: const SizedBox(
+                      width: 200,
+                      height: 300,
+                      child: Card(
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("You Tapped the red box");
+                    },
+                    child: const SizedBox(
+                      width: 200,
+                      height: 300,
+                      child: Card(
+                        color: Colors.yellow,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("You Tapped the red box");
+                    },
+                    child: const SizedBox(
+                      width: 200,
+                      height: 300,
+                      child: Card(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
-
-
-
-/*
-  SAYFALAR ARASI GEÇİŞ
-void navigateToNextScreen(BuildContext context) {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => HomePage()));
-  }*/ 
--- test_area/lib/main.dart
+++ test_area/lib/main.dart
@@ -1,44 +1,22 @@
-// ignore_for_file: deprecated_member_use
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'home_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   final userNameController = TextEditingController();
-
   final userPasswordController = TextEditingController();
 
   bool isAvalible = false;
-
-  
-
-  void isSame() {
-
-    final enteredUserName = userNameController.text;
-    final enteredPassword = userPasswordController.text;
-
-    if ((enteredUserName == "test@test.com") &&
-        (enteredPassword == "password")) {
-      userNameController.text = "";
-      userPasswordController.text = "";
-      isAvalible = true;
-      // ignore: avoid_print
-      return print("Welcome");
-    } 
-    else 
-    {
-      userNameController.text = "";
-      userPasswordController.text = "";
-      // ignore: avoid_print
-      return print("Error");
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +24,6 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          brightness: Brightness.dark,
           title: const Text(
             "Welcome The Cinema App!",
             style: TextStyle(
@@ -55,17 +32,18 @@ class _MyAppState extends State<MyApp> {
           ),
           backgroundColor: Colors.transparent,
           centerTitle: true,
-          elevation: 0.0,
+          elevation: 0.0, 
+          systemOverlayStyle: SystemUiOverlayStyle.light,
         ),
         body: SingleChildScrollView(
           child: Container(
-            margin: const EdgeInsets.all(15),
+            margin: const EdgeInsets.all(20),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Image.asset("assets/images/loginScreen.png"),
-                const SizedBox(height: 20),
+                const SizedBox(height: 10),
                 TextField(
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
@@ -74,7 +52,6 @@ class _MyAppState extends State<MyApp> {
                   ),
                   keyboardType: TextInputType.emailAddress,
                   controller: userNameController,
-                  onSubmitted: (_) => isSame(),
                 ),
                 const SizedBox(height: 10),
                 TextField(
@@ -85,37 +62,53 @@ class _MyAppState extends State<MyApp> {
                     labelText: 'Password',
                   ),
                   controller: userPasswordController,
-                  onSubmitted: (_) => isSame(),
                 ),
                 const SizedBox(height: 10),
                 ConstrainedBox(
-                  constraints:const BoxConstraints.tightFor(width: 300, height: 50),
-                  child: ElevatedButton(
-                    onPressed: () => isSame(),
-                    child: const Text('Login'),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red,
-                      onPrimary: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25), // <-- Radius
+                  constraints:
+                      const BoxConstraints.tightFor(width: 300, height: 50),
+                  child: Builder(builder: (context) {
+                    return ElevatedButton(
+                      onPressed: () => {
+                        if (userNameController.text == "test@test.com" &&
+                            userPasswordController.text == "password")
+                          {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePage()))
+                          }
+                        else
+                          {
+                            // ignore: avoid_print
+                            print("Hatalı Giriş")
+                          }
+                      },
+                      child: const Text('Login'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                        onPrimary: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
                       ),
-                    ),
-                  ),
+                    );
+                  }),
                 ),
                 const SizedBox(height: 10),
-                FlatButton(
+                TextButton(
+                  onPressed: () {},
                   child: const Text(
-                    "Forgot Password!",
+                    "Sign Up",
                     style: TextStyle(
                       color: Colors.red,
                     ),
                   ),
-                  onPressed: isSame,
                 ),
-                FlatButton(
+                TextButton(
                   onPressed: () {},
                   child: const Text(
-                    "Forgot Email!",
+                    "Forgot Password!",
                     style: TextStyle(
                       color: Colors.red,
                     ),
