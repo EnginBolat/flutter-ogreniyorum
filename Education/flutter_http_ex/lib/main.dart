e59f83185b47adcdca93467d034e4c6ed5636b25
Engin Bolat <engi.bolat@gmail.com>
2022-03-24 11:08:04 +0300

    Listed

--- Education/flutter_http_ex/lib/main.dart
+++ Education/flutter_http_ex/lib/main.dart
@@ -42,30 +42,32 @@ class _MyAppState extends State<MyApp> {
     return parseKisilerCevap(cevap.body);
   }
 
-   Future<void> kisiSil(int kisi_id) async {
-    var url =
-        Uri.parse("http://kasimadalan.pe.hu/kisiler/delete_kisiler.php");
+  Future<void> kisiSil(int kisi_id) async {
+    var url = Uri.parse("http://kasimadalan.pe.hu/kisiler/delete_kisiler.php");
     var veri = {"kisi_id": kisi_id.toString()};
     var cevap = await http.post(url, body: veri);
-      // ignore: avoid_print
+    // ignore: avoid_print
     print("Silme Cevap:${cevap.body}");
   }
 
-  Future<void> kisiEkle(String kisi_ad,String kisi_tel) async {
-    var url =
-        Uri.parse("http://kasimadalan.pe.hu/kisiler/insert_kisiler.php");
-    var veri = {"kisi_ad": kisi_ad,"kisi_tel":kisi_tel};
+  Future<void> kisiEkle(String kisi_ad, String kisi_tel) async {
+    var url = Uri.parse("http://kasimadalan.pe.hu/kisiler/insert_kisiler.php");
+    var veri = {"kisi_ad": kisi_ad, "kisi_tel": kisi_tel};
     var cevap = await http.post(url, body: veri);
-      // ignore: avoid_print
+    // ignore: avoid_print
     print("Kayıt Cevap:${cevap.body}");
   }
 
-Future<void> kisiGuncelle(int kisi_id,String kisi_ad,String kisi_tel) async {
-    var url =
-        Uri.parse("http://kasimadalan.pe.hu/kisiler/update_kisiler.php");
-    var veri = {"kisi_id":kisi_id.toString(),"kisi_ad": kisi_ad,"kisi_tel":kisi_tel};
+  Future<void> kisiGuncelle(
+      int kisi_id, String kisi_ad, String kisi_tel) async {
+    var url = Uri.parse("http://kasimadalan.pe.hu/kisiler/update_kisiler.php");
+    var veri = {
+      "kisi_id": kisi_id.toString(),
+      "kisi_ad": kisi_ad,
+      "kisi_tel": kisi_tel
+    };
     var cevap = await http.post(url, body: veri);
-      // ignore: avoid_print
+    // ignore: avoid_print
     print("Güncelleme Cevap:${cevap.body}");
   }
 
@@ -88,36 +90,59 @@ Future<void> kisiGuncelle(int kisi_id,String kisi_ad,String kisi_tel) async {
   void initState() {
     super.initState();
     // kisileriGoster();
-    // kisiSil(4842);
+    // kisiSil(4852);
     // kisiEkle("eng", "2224");
-    kisiGuncelle(4847, "engin", "2224");
+    // kisiGuncelle(4847, "engin", "2224");
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      title: 'HTTP APP',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter HTTP'),
+          centerTitle: true,
         ),
-        body: Center(
-            child: ListView.builder(
-                itemCount: listeUzunluk,
+        body: FutureBuilder<List<Kisiler>>(
+          future: tumKisiler(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var kisiListesi = snapshot.data;
+              return ListView.builder(
+                itemCount: kisiListesi!.length,
                 itemBuilder: (context, index) {
+                  var kisi = kisiListesi[index];
                   return SizedBox(
-                    height: 150,
-                    width: double.infinity,
+                    height: 80,
                     child: Card(
-                        child: Column(
-                      children: const <Widget>[
-                        Text("Kisi Id:"),
-                        Text("Kisi Ad:"),
-                        Text("Kisi Tel:")
-                      ],
-                    )),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Kişi Id: ${kisi.kisi_id}",
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            "Kişi Adı: ${kisi.kisi_ad}",
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            "Kişi Tel: ${kisi.kisi_tel}",
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                    ),
                   );
-                })),
+                },
+              );
+            } else {
+              return const Center();
+            }
+          },
+        ),
       ),
     );
   }
